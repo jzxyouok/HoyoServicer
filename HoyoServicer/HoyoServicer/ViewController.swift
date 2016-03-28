2dab4dcf7ddcbdc9eb8f8dcdcb72ace842ebad1e
zhaowuxie <545691639@qq.com>
2016-03-28 11:32:04 +0800

    新建的项目

---
+++ HoyoServicer/HoyoServicer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HoyoServicer
+//
+//  Created by 赵兵 on 16/3/28.
+//  Copyright © 2016年 com.ozner.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
