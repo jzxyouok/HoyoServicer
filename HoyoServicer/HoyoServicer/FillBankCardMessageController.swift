22e9db0a2dc3fad1a086317928bbcf57aebf0a25
zhaowuxie <545691639@qq.com>
2016-04-10 22:24:53 +0800

    gengxin

--- HoyoServicer/HoyoServicer/FillBankCardMessageController.swift
+++ HoyoServicer/HoyoServicer/FillBankCardMessageController.swift
@@ -47,14 +47,6 @@ class FillBankCardMessageController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }
