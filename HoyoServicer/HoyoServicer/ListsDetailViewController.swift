a806efc0fbcba9cd36ba7f0cd0ea340943cc1264
Mrzhouzhouyang <yanglongzhou@yanglongzhous-macbook-pro.local>
2016-04-15 10:49:29 +0800

    杨龙洲

--- HoyoServicer/HoyoServicer/ListsDetailViewController.swift
+++ HoyoServicer/HoyoServicer/ListsDetailViewController.swift
@@ -98,15 +98,7 @@ class ListsDetailViewController: UITableViewController {
     }
     */
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
    
 
 }
